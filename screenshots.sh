4b3f874b1adaca7975fdee2c80206de65a4e9504
razigli <razigli@artix.dev>
2026-09-14 15:06:39 +0500

    optimize screenshots.sh

--- screenshots.sh
+++ screenshots.sh
@@ -2,20 +2,16 @@
 lines=""
 dir="$(xdg-user-dir PICTURES)/Screenshots"
 
-count="$(ls -1 $dir | wc -l)"
-if [[ "$count" == "0" ]]; then
-	exit 0
-fi
-for file in $dir/*; do
-	if [ -z "$file" ]; then
-		exit 0
-	fi
-	lines="$(basename $file)\0icon\x1f$file\n$lines"
+for file in $dir/*.png; do
+	[[ -f "$file" ]] &&
+		filename="$(basename $file)" \
+		lines="$filename\0icon\x1f$file\n$lines"
 done
 
-selected="$(echo -en "$lines" | PREVIEW=true rofi -dmenu -theme preview.rasi)"
+export PREVIEW=true
+selected="$(echo -en "$lines" | rofi -dmenu -theme preview.rasi)"
 
 if [ -n "$selected" ]; then
 	satty --filename "$dir/$selected" --output-filename "$dir/selected"
-	cat "$dir/$selected" | wl-copy  -t image/png
+	cat "$dir/$selected" | wl-copy -t image/png
 fi
